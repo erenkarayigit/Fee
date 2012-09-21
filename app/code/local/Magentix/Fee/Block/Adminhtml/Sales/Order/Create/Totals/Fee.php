ff4bef2610ba26ad4b13f90ea35b9cc3a13f7aa4
magentix <contact@magentix.fr>
2012-09-21 17:11:02 +0200

    logic moved

---
+++ app/code/local/Magentix/Fee/Block/Adminhtml/Sales/Order/Create/Totals/Fee.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by Magentix
+ * Based on Module from "Excellence Technologies" (excellencetechnologies.in)
+ *
+ * @category   Magentix
+ * @package    Magentix_Fee
+ * @author     Matthieu Vion (http://www.magentix.fr)
+ * @license    This work is free software, you can redistribute it and/or modify it
+ */
+
+class Magentix_Fee_Block_Adminhtml_Sales_Order_Create_Totals_Fee extends Mage_Adminhtml_Block_Sales_Order_Create_Totals_Default
+{
+    /**
+     * Use your own template if necessary
+     * See "sales/order/create/totals/default.phtml" for model
+     */
+    // protected $_template = 'fee/sales/order/create/totals/fee.phtml';
+}
---
+++ app/code/local/Magentix/Fee/Block/Checkout/Totals/Fee.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by Magentix
+ * Based on Module from "Excellence Technologies" (excellencetechnologies.in)
+ *
+ * @category   Magentix
+ * @package    Magentix_Fee
+ * @author     Matthieu Vion (http://www.magentix.fr)
+ * @license    This work is free software, you can redistribute it and/or modify it
+ */
+
+class Magentix_Fee_Block_Checkout_Totals_Fee extends Mage_Checkout_Block_Total_Default
+{
+    /**
+     * Use your own template if necessary
+     * See "checkout/total/default.phtml" for model
+     */
+    // protected $_template = 'fee/checkout/total/fee.phtml';
+}
